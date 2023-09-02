b795488d265f4933eae98ea045a38f1aa4de22e3
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-09-02 12:24:27 +0330

    create main activity class as app entry point

---
+++ app/src/main/java/ai/bale/musicplayer/MainActivity.kt
@@ -0,0 +1,16 @@
+package ai.bale.musicplayer
+
+import ai.bale.musicplayer.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
