13b4199784c2d819c93c57b329cf4dd83ee97b79
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-09-03 11:35:03 +0330

    prepare player fragments that setup player controllers and handle media playing

--- app/src/main/java/ai/bale/musicplayer/fragments/PlayerFragment.kt
+++ app/src/main/java/ai/bale/musicplayer/fragments/PlayerFragment.kt
@@ -1,31 +1,32 @@
 package ai.bale.musicplayer.fragments
 
+import ai.bale.musicplayer.R
 import ai.bale.musicplayer.databinding.PlayerFragmentBinding
+import ai.bale.musicplayer.services.PlayerProvider
 import ai.bale.musicplayer.services.PlayerService
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 
-class PlayerFragment(): Fragment() {
+class PlayerFragment : Fragment() {
     private lateinit var binding: PlayerFragmentBinding
-    private lateinit var playerService: PlayerService
-
-    private val serviceConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as PlayerService.ServiceBinder
-            playerService = binder.getPlayerService()
-            initUI()
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {}
-    }
+    private lateinit var player: ExoPlayer
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,45 +39,129 @@ class PlayerFragment(): Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val intent = Intent(requireContext(), PlayerService::class.java)
-        requireContext().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        player = PlayerProvider.Player
+        setPlayerControls()
     }
 
-    private fun initUI() {
-        binding.playerPlayButton.setOnClickListener {
-            if (!playerService.playing()) {
-                playerService.play()
-            }else{
-                playerService.pause()
+    private fun setPlayerControls() {
+        player.addListener(object : Player.Listener {
+            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+                super.onMediaItemTransition(mediaItem, reason)
+                if (mediaItem == null) return
+                binding.playerMusicTitle.text = mediaItem.mediaMetadata.title
+                binding.playerProgressBar.progress = player.currentPosition.toInt()
+                binding.playerProgressBar.max = player.duration.toInt()
+                binding.playerMusicSinger.text = mediaItem.mediaMetadata.albumArtist
+
+                binding.playerMusicCover.setImageURI(mediaItem.mediaMetadata.artworkUri)
+                if (binding.playerMusicCover.drawable == null) {
+                    binding.playerMusicCover.setImageResource(R.drawable.cover_music)
+                }
+
+                binding.playerPlayButton.setImageResource(R.drawable.pause_icon)
+                updateMediaProgress()
+
+                if (!player.isPlaying){
+                    player.play()
+                }
             }
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                if (playbackState == ExoPlayer.STATE_READY){
+                    val currentMedia = player.currentMediaItem ?: return
+                    binding.playerMusicTitle.text = currentMedia.mediaMetadata.title
+                    binding.playerProgressBar.progress = player.currentPosition.toInt()
+                    binding.playerProgressBar.max = player.duration.toInt()
+                    binding.playerMusicSinger.text = currentMedia.mediaMetadata.artist ?: "Artist"
+                    binding.playerPlayButton.setImageResource(R.drawable.pause_icon)
+
+                    binding.playerMusicCover.setImageURI(currentMedia.mediaMetadata.artworkUri)
+                    if (binding.playerMusicCover.drawable == null) {
+                        binding.playerMusicCover.setImageResource(R.drawable.cover_music)
+                    }
+
+
+                    updateMediaProgress()
+                }else{
+                    binding.playerPlayButton.setImageResource(R.drawable.play_icon)
+                }
+            }
+        })
+
+        binding.playerNextButton.setOnClickListener { skipNext() }
+        binding.playerPrevButton.setOnClickListener { skipPrevious() }
+        binding.playerPlayButton.setOnClickListener { playOrPause() }
+        setSeekBarListener()
+
+        binding.playerMusicTitle.isSelected = true
+        binding.playerMusicTitle.isSingleLine = true
+        binding.playerMusicTitle.ellipsize = TextUtils.TruncateAt.MARQUEE
+        binding.playerMusicTitle.marqueeRepeatLimit = -1
+        binding.playerMusicTitle.isFocusable = true
+        binding.playerMusicTitle.isFocusableInTouchMode = true
+        binding.playerMusicTitle.requestFocus()
+    }
+
+    private fun updateMediaProgress() {
+        val handler = Handler().postDelayed({
+            if (player.isPlaying) {
+                binding.playerProgressBar.progress = player.currentPosition.toInt()
+            }
+            updateMediaProgress()
+        }, 1000)
+    }
+
+    private fun skipNext(){
+        if (player.hasNextMediaItem()){
+            player.seekToNext()
         }
-
-        updateUI(playerService.playing() ?: false)
     }
 
-    private fun updateUI(isPlaying: Boolean) {
-        if (isPlaying) {
-            binding.playerPauseButton.visibility = View.VISIBLE
-            binding.playerPlayButton.visibility = View.GONE
-        } else {
-            binding.playerPauseButton.visibility = View.GONE
-            binding.playerPlayButton.visibility = View.VISIBLE
+    private fun skipPrevious(){
+        if (player.hasPreviousMediaItem()){
+            player.seekToPrevious()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        requireContext().unbindService(serviceConnection)
+    private fun playOrPause(){
+        if (player.isPlaying){
+            player.pause()
+            binding.playerPlayButton.setImageResource(R.drawable.play_icon)
+            return
+        }
+        player.play()
+        binding.playerPlayButton.setImageResource(R.drawable.pause_icon)
     }
 
-    override fun onResume() {
-        super.onResume()
-        val intent = Intent(requireContext(), PlayerService::class.java)
-        requireContext().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    private fun setSeekBarListener() {
+        binding.playerProgressBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            var progress = 0
+            var isTracking = false
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (seekBar == null || !fromUser) {
+                    return
+                }
+                this.progress = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                isTracking = true
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar == null) {
+                    return
+                }
+                isTracking = false
+                player.seekTo(progress.toLong())
+                if (!player.isPlaying) {
+                    player.play()
+                }
+            }
+        })
     }
 
-    override fun onPause() {
-        super.onPause()
-        requireContext().unbindService(serviceConnection)
-    }
 }
